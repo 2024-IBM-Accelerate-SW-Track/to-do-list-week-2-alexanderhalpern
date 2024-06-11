72e81a13ae753b9ffdea614a2b3d5983d9eb3066
Alexander Halpern <halperna22@gmail.com>
2024-06-10 22:45:54 -0400

    Finished

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import alexanderhalpern from "../assets/alexanderhalpern.jpg";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                src={alexanderhalpern}
+                style={{ width: 300 }}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Alexander Halpern</div>
+              <div className="brief_description">
+                Rodman Scholar at University of Virginia. I play piano for the
+                UVA Jazz Ensemble!
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
